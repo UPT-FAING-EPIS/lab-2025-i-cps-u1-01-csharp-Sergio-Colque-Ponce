fad1968dfa805afe38d6b2c66abf3a4427d2d7c2
Sergio-Colque-Ponce <younever.tlv@gmail.com>
2025-03-27 08:20:19 -0500

    prueba1

---
+++ Bank/Bank.WebApi.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace Bank.WebApi.Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
